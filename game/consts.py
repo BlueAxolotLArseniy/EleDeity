f212b8189f84d639282648e902b119e73466c78d
BlueAxolotLArseniy <arseny.ant.lykov@gmail.com>
2026-02-15 20:36:34 +0600

    added: better window

--- game/consts.py
+++ game/consts.py
@@ -3,11 +3,15 @@ import game.func_common as func_common
 WINDOW_PROPORTIONS = 16 / 9
 WINDOW_SCALE = 0.5
 
-WIDTH = func_common.get_screen_resolution()[0] * WINDOW_SCALE
-HEIGHT = WIDTH / WINDOW_PROPORTIONS
+phys_w, phys_h, scale = func_common.get_screen_specs()
+
+WIDTH = int((phys_w / 2) / scale)
+HEIGHT = int((phys_h / 2) / scale)
 
 PROGRAM_TITLE = "EleDeity"
 
 PLAYER_MOVEMENT_SPEED = 6
 GRAVITY = -1
 PLAYER_MOVEMENT_SLOWDOWN = 0.95
+
+# func_common.get_monitor_scale()
--- game/func_common.py
+++ game/func_common.py
@@ -1,8 +1,30 @@
 import ctypes
 
-
 def get_screen_resolution() -> tuple[float, float]:
     ctypes.windll.user32.SetProcessDPIAware()
     width = ctypes.windll.user32.GetSystemMetrics(0)
     height = ctypes.windll.user32.GetSystemMetrics(1)
     return width, height
+
+def get_screen_specs():
+    """
+    Возвращает реальное разрешение экрана и коэффициент масштабирования (Scale).
+    """
+    try:
+        ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    except Exception:
+        ctypes.windll.user32.SetProcessDPIAware()
+
+    user32 = ctypes.windll.user32
+    gdi32 = ctypes.windll.gdi32
+
+    physical_width = user32.GetSystemMetrics(0)
+    physical_height = user32.GetSystemMetrics(1)
+
+    hdc = user32.GetDC(0)
+    dpi = gdi32.GetDeviceCaps(hdc, 88)
+    user32.ReleaseDC(0, hdc)
+
+    scale = dpi / 96.0
+
+    return physical_width, physical_height, scale
